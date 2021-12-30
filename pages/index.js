3964c99ebc7a2100e8080e3998e9dc11d923db21
kamruz-zaman8991 <86624274+kamruz-zzaman@users.noreply.github.com>
2021-12-31 00:38:48 +0600

    done nav and foot

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import Footer from "../Component/Footer/Footer";
 import Navbar from "../Component/Header/Navbar";
+import Login from "../Component/Login/Login";
+import useAuth from "../Hooks/useAuth";
 
 
 export default function Home() {
+  const { user } = useAuth()
+  console.log(user);
   return (
-    <div>
-      <Navbar></Navbar>
-      <Footer></Footer>
-
-    </div>
+    <>
+      {
+        user.email ? <><Navbar></Navbar>
+          <Footer></Footer></> : <Login></Login>
+      }
+    </>
   )
 }
