8a3b63775f0270d56f6ed816780ce66669dfdc07
kamruz-zaman8991 <86624274+kamruz-zzaman@users.noreply.github.com>
2021-12-30 13:20:11 +0600

    login form added

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
     <div>
       <Navbar></Navbar>
       <Footer></Footer>
+
     </div>
   )
 }
---
+++ pages/login.js
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import React from 'react';
+import logo from '../Images/company_logo.png'
+import googleLogo from '../Images/google-logo.png'
+
+const login = () => {
+    return (
+        <>
+            <div className='flex justify-center items-center' >
+                <div className='border-2 px-10 py-16 m-10 rounded-lg'>
+                    <div className='flex justify-center items-center mt-5'>
+                        <Image width="120px" height="120px" className='' src={logo} alt="" />
+                    </div>
+                    <div>
+                        <p className='font-bold text-3xl m-5 text-center'>Login With</p>
+                        <button className=' ml-8 my-5 border-2 py-1 text-center px-7 rounded-3xl'><Image width='20px' height='20px' className=' inline  ' src={googleLogo} alt="" /> Continue with Google </button>
+                        <p>Don't have an account? <a href="https://accounts.google.com/signup/v2/webcreateaccount?hl=en&flowName=GlifWebSignIn&flowEntry=SignUp" target='_blank' rel="noreferrer" className='text-blue-400 border-b  border-blue-300'>Create an account</a> </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default login;
